e21734178f09b67f1a61c9b12476a2a36247f705
Kraftey <info.kraftey@gmail.com>
2025-09-27 13:40:07 +0530

    Add Google Tag Manager (GTM-T4CSKQJF) to all routes

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import Providers from '../components/Providers'
+import Analytics from '../components/Analytics'
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://kraftey.com'),
@@ -246,44 +247,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11324883721"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'AW-11324883721');
-              
-              // Conversion tracking function
-              function gtag_report_conversion(url) {
-                var callback = function () {
-                  if (typeof(url) != 'undefined') {
-                    window.location = url;
-                  }
-                };
-                gtag('event', 'conversion', {
-                    'send_to': 'AW-11324883721/7OqlCMnpl_0ZEImGkJgq',
-                    'value': 1.0,
-                    'currency': 'INR',
-                    'event_callback': callback
-                });
-                return false;
-              }
-            `
-          }}
-        />
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T4CSKQJF');`
+        }} />
+        {/* End Google Tag Manager */}
         
         {/* Google AdSense Meta Tag */}
         <meta name="google-adsense-account" content="ca-pub-9525032831778850" />
         
-        {/* Google AdSense Script */}
-        <script 
-          async 
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9525032831778850"
-          crossOrigin="anonymous"
-        ></script>
+        {/* Quill.js CDN */}
+        <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet" />
+        <script src="https://cdn.quilljs.com/1.3.6/quill.min.js"></script>
         
         {/* Structured Data */}
         {jsonLd.map((schema, index) => (
@@ -295,6 +274,18 @@ export default function RootLayout({
         ))}
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T4CSKQJF"
+            height="0" 
+            width="0" 
+            style={{display: 'none', visibility: 'hidden'}}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
+        <Analytics />
         <Providers>{children}</Providers>
       </body>
     </html>
