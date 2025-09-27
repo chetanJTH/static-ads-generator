515dc689019455fe74a4d7028fab7f922fbcf528
Kraftey <info.kraftey@gmail.com>
2025-09-27 17:48:36 +0530

    Force deployment to fix blog posts display

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -247,7 +247,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager - Force deployment */}
+        {/* Google Tag Manager - Force deployment - Fix blog posts */}
         <script dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
